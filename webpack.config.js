64418a021988ee8c5ce109304a36cfcf6c199602
enestekin <86486997+enestekin@users.noreply.github.com>
2021-06-26 16:10:09 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+        path: path.resolve(__dirname, "public/scripts"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        publicPath: "/scripts/",
+    },
+    watch: true,
+    devtool: "source-map"
+};
